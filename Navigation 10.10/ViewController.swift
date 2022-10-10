2b68af097217cb1abc73c02af155a9454d4a5706
galador2 <galador2@yandex.ru>
2022-10-10 15:25:39 +0300

    Initial Commit

---
+++ Navigation 10.10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation 10.10
+//
+//  Created by Kirill  Kostenko  on 10.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
