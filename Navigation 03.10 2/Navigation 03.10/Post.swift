93e7fd59f7114e7d4aeeb8adb4b99b1576ca17ec
galador2 <109518778+galador2@users.noreply.github.com>
2022-10-10 15:50:05 +0300

    Add files via upload

---
+++ Navigation 03.10 2/Navigation 03.10/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  Navigation 03.10
+//
+//  Created by Kirill  Kostenko  on 04.10.2022.
+//
+
+import UIKit
+
+struct Post{
+    var title:String
+}
---
+++ Navigation 03.10 2/Navigation 03.10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation 03.10
+//
+//  Created by Kirill  Kostenko  on 03.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
